60b4967f1537c770cda28d126e3a07166617c507
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2018-04-21 22:12:58 +0200

    Authorization working

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,19 @@
 import express from "express";
+import http from "http";
+import spotifyService from "./spotifyService";
 
 const app = express();
+const server = http.createServer(app);
 
-app.listen(3000, () => console.log("Server listening on port 3000!"));
+const io = require("socket.io").listen(server);
+
+app.get("/play", (req, res) => spotifyService.playTrack());
+app.get("/callback", (req, res) => {
+  spotifyService.postInit(req.query.code, io);
+  res.send("AutoDJ started!");
+});
+
+server.listen(3000, () => console.log("Server listening on port 3000!"));
+spotifyService.init();
 
 export default app;
---
+++ server/src/spotifyService/index.js
@@ -0,0 +1,82 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyService = {
+  userCount: 0,
+
+  init: function() {
+    console.log("init");
+
+    const scopes = [
+        "user-read-private",
+        "user-read-email",
+        "playlist-modify-private",
+        "streaming",
+        "user-read-playback-state",
+        "user-read-currently-playing",
+        "user-modify-playback-state"
+      ],
+      redirectUri = "http://localhost:3000/callback",
+      clientId = "bec599db35c646c498f4d4b865415a1d",
+      clientSecret = "81b778cae9e24ad883e7e25615782f40",
+      state = "some-state-of-my-choice";
+
+    this.spotifyApi = new SpotifyWebApi({
+      redirectUri: redirectUri,
+      clientId: clientId,
+      clientSecret: clientSecret
+    });
+
+    const authorizeURL = this.spotifyApi.createAuthorizeURL(scopes, state);
+
+    console.log(authorizeURL);
+  },
+
+  postInit: function(code, io) {
+    console.log("post init");
+
+    this.spotifyApi.authorizationCodeGrant(code).then(
+      data => {
+        // Set the access token on the API object to use it in later calls
+        this.spotifyApi.setAccessToken(data.body["access_token"]);
+        this.spotifyApi.setRefreshToken(data.body["refresh_token"]);
+
+        this.start(io);
+      },
+      function(err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+  },
+
+  start: function(io) {
+    console.log("start");
+
+    io.on("connection", socket => {
+      this.userCount += 1;
+
+      socket.on("play", trackID => {
+        this.playTrack(trackID);
+      });
+
+      socket.on("disconnect", () => {
+        this.userCount -= 1;
+      });
+    });
+  },
+
+  playTrack(trackID) {
+    console.log(this.spotifyApi.getAccessToken());
+
+    this.spotifyApi.getMyCurrentPlaybackState({}).then(
+      data => {
+        // Output items
+        console.log("Now Playing: ", data.body);
+      },
+      function(err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+  }
+};
+
+export default spotifyService;
